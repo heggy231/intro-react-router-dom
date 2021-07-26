5b0dc1580b39c8c48a36f6aab071e013b365bfef
Heggy Castaneda <heggyy@gmail.com>
2021-07-26 12:50:50 -0400

    props.history.push("/")

--- README.md
+++ README.md
@@ -143,3 +143,28 @@ props = {
 */
   }, []);
 ```
+
+## useHistory
+
+```jsx
+import React from "react";
+import { useHistory } from "react-router";
+
+const Protected = (props) => {
+  const history = useHistory();
+  if (!props.isLoggedIn) {
+    // if not logged in, redirect using history.push back to home
+    history.push("/");
+    return null;
+  } else {
+    // if logged in, show protected blog page
+    return (
+      <>
+        {props.children}
+      </>
+    )
+  }
+};
+
+export default Protected;
+```
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
 
 const MovieDetails = (props) => {
-
   // useEffect to get access to `:movieId`
   useEffect(() => {
-    console.log('props', props);
-    console.log('movid id', props.match.params.movieId);
+    console.log("movid id", props.match.params.movieId);
     /**
  * props = { 
     location: { pathname: "/movies/45" },
@@ -13,11 +11,18 @@ const MovieDetails = (props) => {
   }
   Therefore, when path is /movies/:movieId then we can access it using `props.match.params.movieId`
 */
-  }, []);
+  });
+
+  const handleGoHomeClick = () => {
+    props.history.push("/");
+  }
 
   return (
     <div>
-      <h1>MovieDetails Component Page!</h1>
+      <h1>
+        MovieDetails Component Page!
+        <button onClick={handleGoHomeClick}>Back to Home</button>
+      </h1>
     </div>
   );
 };
