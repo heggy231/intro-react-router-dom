9ed673f62adae901dafaca671b7abc5f83f9c126
Heggy Castaneda <heggyy@gmail.com>
2021-07-30 07:10:43 -0400

    render app in path /foo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './App';
-import AddMovie from './components/AddMovie';
-import BaseLayout from './components/BaseLayout';
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
-import MovieDetails from './components/MovieDetails';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <BaseLayout>
-        <Switch>
-          <Route component={App} path="/" exact />
-          <Route component={AddMovie} path="/add-movie" />
-          <Route component={MovieDetails} path="/movies/:movieId" />
-        </Switch>
-      </BaseLayout>
+      <Route component={App} path="/foo" />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
