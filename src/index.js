e6f3b73e4a86e6d9e6623d3ad6bcc6770c4ddf6a
Heggy Castaneda <heggyy@gmail.com>
2021-07-30 10:51:44 -0400

    wrap BaseLayout inside index routes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './App';
 import AddMovie from './components/AddMovie';
+import BaseLayout from './components/BaseLayout';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route component={App} path="/" exact />
-      <Route component={AddMovie} path="/movie" exact />
+
+      <BaseLayout>
+        <Route component={App} path="/" exact />
+        <Route component={AddMovie} path="/movie" exact />
+      </BaseLayout>
+
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
