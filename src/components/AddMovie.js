ef976691ae3475a1c1a1c5e31e80c9607be72196
Heggy Castaneda <heggyy@gmail.com>
2021-07-30 07:17:25 -0400

    simple routing

---
+++ src/components/AddMovie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddMovie = () => {
+  return (
+    <>
+      <h1>Add Movie</h1>
+    </>
+  )
+}
+
+export default AddMovie;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './App';
+import AddMovie from './components/AddMovie';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route component={App} path="/foo" />
+      <Route component={App} path="/" exact />
+      <Route component={AddMovie} path="/movie" exact />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
