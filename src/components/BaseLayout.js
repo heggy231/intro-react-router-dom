4eac5698b170997993d0f65f2fcc90256408f3a2
Heggy Castaneda <heggyy@gmail.com>
2021-07-30 10:47:34 -0400

    setup base

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BaseLayout = (props) => {
+  return (
+    <>
+      <header>Header</header>
+      <main>Content</main>
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default BaseLayout;
